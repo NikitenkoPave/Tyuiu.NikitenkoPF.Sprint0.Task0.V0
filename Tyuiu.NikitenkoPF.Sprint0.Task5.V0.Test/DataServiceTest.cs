4b76d8ad1781090817835b027e660887d4ed4e0f
Tyuiu.NikitenkoPF@outlook.com <yasmotruiuzaki@gmail.com>
2023-09-25 19:31:54 +0500

    Task5

---
+++ Tyuiu.NikitenkoPF.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.NikitenkoPF.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.NikitenkoPF.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+
+        }
+        [TestMethod]
+        public void CheckedSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+
+        }
+
+        [TestMethod]
+        public void CheckedMultiplicationnValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+
+
+        }
+        [TestMethod]
+        public void CheckedDivivsionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+
+        }
+    }
+}
---
+++ Tyuiu.NikitenkoPF.Sprint0.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.NikitenkoPF.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.NikitenkoPF.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("84a2b786-3e5d-4611-a236-7750e497f57e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
